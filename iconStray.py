3d1589595978c74039f8e5fd0fcd5e3a2a8a8c32
waticouz <119053268+wateecuhs@users.noreply.github.com>
2023-01-09 17:39:10 +0100

    changed how icon stray works
    doesnt run/stops the icon every usage, it was quite dumb. 
    Now always running with a "minimize app"/"show app" button.
    not everything is implemented yet

--- iconStray.py
+++ iconStray.py
@@ -1,11 +1,13 @@
+import threading
 from pystray import Menu, MenuItem, Icon
 import os
 import sys
 import winshell
 from PIL import Image
-import threading
+from main import show_app
 
 image = Image.open("assets/favicon.ico")
+icon_started = False
 
 
 def on_clicked1(item):
@@ -30,15 +32,16 @@ def on_clicked2(item):
     state2 = True
 
 
-def show_window(icon, item):
-    global app
-    icon.stop()
-    app.deiconify()
+def show_window():
+    global window_text
+    show_app()
+    window_text = "Minimize app"
 
 
-def quit_window(icon, item):
-    global app
+def quit_window():
+    global app, process
     icon.stop()
+    process.kill()
     app.destroy()
     os._exit(1)
 
@@ -57,27 +60,37 @@ def get_paths():
         state2 = True
 
 
+class IconThread(threading.Thread):
+
+    def __init__(self, *icon_args, **icon_kwargs):
+        self.icon = None
+        self._icon_args = icon_args
+        self._icon_kwargs = icon_kwargs
+
+        threading.Thread.__init__(self, daemon=True)
+
+    def run(self):
+        self.icon = Icon(*self._icon_args, **self._icon_kwargs)
+        self.icon.run()
+
+    def stop(self):
+        if self.icon:
+            self.icon.stop()
+
+
 def init_icon():
     global icon
     get_paths()
-    icon = Icon('test', image, menu=Menu(MenuItem(
-        'Show window',
-        show_window)
-        , MenuItem(
-            'Run on startup',
-            Menu(
-                MenuItem(
-                    'Enable',
-                    on_clicked1,
-                    checked=lambda item1: state1),
-                MenuItem(
-                    'Disable',
-                    on_clicked2,
-                    checked=lambda item2: state2))),
-        MenuItem(
-            'Exit the program',
-            quit_window)))
+    window_text = 'idk yet lole'
+    icon = IconThread('Riot Account Manager', image, menu=Menu(
+        MenuItem((lambda text: window_text), show_window),
+        MenuItem('Run on startup', Menu(
+            MenuItem('Enable', on_clicked1, checked=lambda item1: state1),
+            MenuItem('Disable', on_clicked2, checked=lambda item2: state2))),
+        MenuItem('Exit the program', quit_window)))
 
 
 def run_icon():
-    icon.run_detached()
+    global window_text
+    if not icon.is_alive():
+        icon.start()
