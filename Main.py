83cf90abcbe4466159763a99341977277f2d6049
waticouz <119053268+wateecuhs@users.noreply.github.com>
2023-01-09 17:37:31 +0100

    update
    made some stuff work and changed how icon stray works (changes mostly in iconStray.py)
    not fully working at all, stopped coding in the middle of fixing something

--- Main.py
+++ Main.py
@@ -29,12 +29,12 @@ class App(customtkinter.CTk):
 
         path = os.path.join(os.path.dirname(sys.argv[0]), 'accounts.json')
         if os.path.exists(path):
-            with open("accounts.json", "r") as accfile:
-                accounts = json.load(accfile)
+            with open("accounts.json", "r") as account_file:
+                accounts = json.load(account_file)
         else:
-            with open("accounts.json", "w") as create_accfile:
+            with open("accounts.json", "w") as create_account_file:
                 accounts = []
-                json.dump(accounts, create_accfile)
+                json.dump(accounts, create_account_file)
 
         customtkinter.deactivate_automatic_dpi_awareness()
         super().__init__()
@@ -50,10 +50,10 @@ class App(customtkinter.CTk):
         window_rect_2 = (window_rect[3] - window_rect[1]) * 0.05
         self.geometry(f"340x400+{int(window_rect[0] + window_rect_1)}+{window_rect_2 + window_rect[1]}")
 
-        self.button_addaccount = customtkinter.CTkButton(master=self, text="Manage profiles",
-                                                         command=self.create_window, fg_color="#125748",
-                                                         hover_color="#1f7d69")
-        self.button_addaccount.grid(row=0, column=1, padx=(10, 20), pady=15)
+        self.button_add_account = customtkinter.CTkButton(master=self, text="Manage profiles",
+                                                          command=self.create_window, fg_color="#125748",
+                                                          hover_color="#1f7d69")
+        self.button_add_account.grid(row=0, column=1, padx=(10, 20), pady=15)
 
         for x in range(len(accounts)):
             customtkinter.CTkButton(self, text=accounts[x].get('profile'), command=lambda number=x: login(number)
@@ -66,22 +66,17 @@ class App(customtkinter.CTk):
         window.grid_columnconfigure((0, 1), weight=0)
         window.grid_rowconfigure((0, 1), weight=0)
         window.title("Manage profiles")
-        customtkinter.CTkButton(window, text="Add a profile", command=self.add_account_window).grid(column=0, row=0,
-                                                                                                    columnspan=2,
-                                                                                                    padx=10, pady=10,
-                                                                                                    sticky="ns")
-        customtkinter.CTkButton(window, text="Remove a profile", command=self.remove_account).grid(column=1, row=1,
-                                                                                                   padx=10, pady=10,
-                                                                                                   sticky="nsew")
-        editprofile = customtkinter.CTkButton(window, text="Edit a profile", command=self.edit_account).grid(column=0,
-                                                                                                             row=1,
-                                                                                                             padx=10,
-                                                                                                             pady=10,
-                                                                                                             sticky="nsew")
-        with open("accounts.json", "r") as accfile:
-            accounts = json.load(accfile)
+        customtkinter.CTkButton(window, text="Add a profile", command=self.add_account_window
+                                ).grid(column=0, row=0, columnspan=2, padx=10, pady=10, sticky="ns")
+        remove_profile = customtkinter.CTkButton(window, text="Remove a profile", command=self.remove_account
+                                                 ).grid(column=1, row=1, padx=10, pady=10, sticky="nsew")
+        edit_profile = customtkinter.CTkButton(window, text="Edit a profile", command=self.edit_account
+                                               ).grid(column=0, row=1, padx=10, pady=10, sticky="nsew")
+        with open("accounts.json", "r") as account_file:
+            accounts = json.load(account_file)
         if len(accounts) == 0:
-            editprofile.configure(state="disabled")
+            edit_profile.configure(state="disabled")
+            remove_profile.configure(state="disabled")
 
     def edit_account_window(self):
         global window3, entry_edit1, entry_edit2, entry_edit3
@@ -93,37 +88,30 @@ class App(customtkinter.CTk):
 
         entry_edit1 = customtkinter.CTkEntry(window3, placeholder_text="Type in the profile name you want to use").grid(
             column=0, row=0, columnspan=2, padx=10, pady=10, sticky="nsew")
-        entry_edit2 = customtkinter.CTkEntry(window3, placeholder_text="Type in your account's username").grid(column=0,
-                                                                                                               row=1,
-                                                                                                               columnspan=2,
-                                                                                                               padx=10,
-                                                                                                               pady=10,
-                                                                                                               sticky="nsew")
-        entry_edit3 = customtkinter.CTkEntry(window3, placeholder_text="Type in your password", show="*").grid(column=0,
-                                                                                                               row=2,
-                                                                                                               columnspan=2,
-                                                                                                               padx=10,
-                                                                                                               pady=10,
-                                                                                                               sticky="nsew")
-        customtkinter.CTkButton(window3, text="Add Account", command=updateacc).grid(column=0, row=3, columnspan=2,
-                                                                                     padx=10, pady=10, sticky="ns")
+        entry_edit2 = customtkinter.CTkEntry(window3, placeholder_text="Type in your account's username"
+                                             ).grid(column=0, row=1, columnspan=2, padx=10, pady=10, sticky="nsew")
+        entry_edit3 = customtkinter.CTkEntry(window3, placeholder_text="Type in your password", show="*"
+                                             ).grid(column=0, row=2, columnspan=2, padx=10, pady=10, sticky="nsew")
+        customtkinter.CTkButton(window3, text="Add Account", command=edit_update
+                                ).grid(column=0, row=3, columnspan=2, padx=10, pady=10, sticky="ns")
 
     def remove_account(self):
-        with open("accounts.json", "r") as accfile:
-            accounts = json.load(accfile)
+        with open("accounts.json", "r") as account_file:
+            accounts = json.load(account_file)
         customtkinter.CTkOptionMenu(master=window, values=[account['profile'] for account in accounts],
-                                    command=optionmenu_callback1).grid(column=1, row=2, padx=10, pady=10, sticky="nsew")
-        customtkinter.CTkButton(master=window, text="Confirm", command=removeaccount).grid(column=1, row=3, padx=10,
-                                                                                           pady=10, sticky="nsew")
+                                    command=option_menu_callback1).grid(column=1, row=2, padx=10, pady=10,
+                                                                        sticky="nsew")
+        customtkinter.CTkButton(master=window, text="Confirm", command=remove_account
+                                ).grid(column=1, row=3, padx=10, pady=10, sticky="nsew")
 
     def edit_account(self):
-        with open("accounts.json", "r") as accfile:
-            accounts = json.load(accfile)
+        with open("accounts.json", "r") as account_file:
+            accounts = json.load(account_file)
         customtkinter.CTkOptionMenu(master=window, values=[account['profile'] for account in accounts],
-                                    command=optionmenu_callback2).grid(column=0, row=2, padx=10, pady=10, sticky="nsew")
-        customtkinter.CTkButton(master=window, text="Confirm", command=self.edit_account_window).grid(column=0, row=3,
-                                                                                                      padx=10, pady=10,
-                                                                                                      sticky="nsew")
+                                    command=option_menu_callback2).grid(column=0, row=2, padx=10, pady=10,
+                                                                        sticky="nsew")
+        customtkinter.CTkButton(master=window, text="Confirm", command=self.edit_account_window
+                                ).grid(column=0, row=3, padx=10, pady=10, sticky="nsew")
 
     def add_account_window(self):
         global window2, entry1, entry2, entry3
@@ -135,91 +123,88 @@ class App(customtkinter.CTk):
 
         entry1 = customtkinter.CTkEntry(window2, placeholder_text="Type in the profile name you want to use").grid(
             column=0, row=0, columnspan=2, padx=10, pady=10, sticky="nsew")
-        entry2 = customtkinter.CTkEntry(window2, placeholder_text="Type in your account's username").grid(column=0,
-                                                                                                          row=1,
-                                                                                                          columnspan=2,
-                                                                                                          padx=10,
-                                                                                                          pady=10,
-                                                                                                          sticky="nsew")
-        entry3 = customtkinter.CTkEntry(window2, placeholder_text="Type in your password", show="*").grid(column=0,
-                                                                                                          row=2,
-                                                                                                          columnspan=2,
-                                                                                                          padx=10,
-                                                                                                          pady=10,
-                                                                                                          sticky="nsew")
-        customtkinter.CTkButton(window2, text="Add Account", command=add_account).grid(column=0, row=3, columnspan=2,
-                                                                                       padx=10, pady=10, sticky="ns")
+        entry2 = customtkinter.CTkEntry(window2, placeholder_text="Type in your account's username"
+                                        ).grid(column=0, row=1, columnspan=2, padx=10, pady=10, sticky="nsew")
+        entry3 = customtkinter.CTkEntry(window2, placeholder_text="Type in your password", show="*"
+                                        ).grid(column=0, row=2, columnspan=2, padx=10, pady=10, sticky="nsew")
+        customtkinter.CTkButton(window2, text="Add Account", command=add_account
+                                ).grid(column=0, row=3, columnspan=2, padx=10, pady=10, sticky="ns")
 
 
 def add_account():
-    pfName = entry1.get()
+    global profile_name
+    profile_name = entry1.get()
     username = entry2.get()
     password = entry3.get()
-    with open("accounts.json", "r") as accfile:
-        accounts = json.load(accfile)
+    with open("accounts.json", "r") as account_file:
+        accounts = json.load(account_file)
     if accounts == "":
         accounts = []
-    profile = {"profile": pfName, "username": username, "password": password}
+    profile = {"profile": profile_name, "username": username, "password": password}
     accounts.append(profile)
-    with open("accounts.json", "w") as accfile:
-        json.dump(accounts, accfile)
+    with open("accounts.json", "w") as account_file:
+        json.dump(accounts, account_file)
     window2.withdraw()
     window.withdraw()
     refresh()
 
 
 def refresh():
-    global app, button_dict
-    customtkinter.CTk.destroy(app)
-    del app
-    time.sleep(3)
+    global app
+    app.withdraw()
+    app.destroy()
+    time.sleep(1)
     app = App()
+    app.protocol('WM_DELETE+_WINDOW', hide_window)
     app.mainloop()
 
 
-def updateacc():
-    global pfName
-    pfName = entry_edit1.get()
+def edit_update():
+    global profile_name
+    profile_name = entry_edit1.get()
     username = entry_edit2.get()
     password = entry_edit3.get()
-    profile = {"profile": pfName, "username": username, "password": password}
-    with open("accounts.json", "r") as accfile:
-        accounts = json.load(accfile)
+    profile = {"profile": profile_name, "username": username, "password": password}
+    with open("accounts.json", "r") as account_file:
+        accounts = json.load(account_file)
     for account in accounts:
         if account_to_edit == account['profile']:
             accounts.remove(account)
     accounts.append(profile)
-    with open("accounts.json", "w") as accfile:
-        json.dump(accounts, accfile)
+    with open("accounts.json", "w") as account_file:
+        json.dump(accounts, account_file)
     window3.withdraw()
     window.withdraw()
     refresh()
 
 
-def removeaccount():
-    with open("accounts.json", "r") as accfile:
-        accounts = json.load(accfile)
+def remove_account():
+    with open("accounts.json", "r") as account_file:
+        accounts = json.load(account_file)
     for account in accounts:
         if account['profile'] == account_to_remove:
             accounts.remove(account)
-    with open("accounts.json", "w") as accfile:
-        json.dump(accounts, accfile)
+    with open("accounts.json", "w") as account_file:
+        json.dump(accounts, account_file)
     window.withdraw()
     refresh()
 
 
-def optionmenu_callback1(choice):
+def option_menu_callback1(choice):
     global account_to_remove
     account_to_remove = choice
 
 
-def optionmenu_callback2(choice):
+def option_menu_callback2(choice):
     global account_to_edit
     account_to_edit = choice
 
 
+def show_app():
+    app.mainloop()
+
+
 def login(index):
-    global app
     lcu_info = LcuInfo()
 
     lcu_port = lcu_info.access_port
@@ -227,8 +212,8 @@ def login(index):
     lcu_password = lcu_info.remoting_auth_token
     lcu_user = 'riot'
 
-    with open("accounts.json", "r") as accfile:
-        accounts = json.load(accfile)
+    with open("accounts.json", "r") as account_file:
+        accounts = json.load(account_file)
 
     payload = {
         'username': accounts[index].get('username'),
@@ -241,13 +226,12 @@ def login(index):
     if response.status_code == 201:
         response_content = response.json()
         if response_content.get('error') == "":
-            isloginavailable = "no"
-            for x in range(len(accounts)):
-                print()
-            hide_window
+            hide_window()
+            print(1)
             wait_for_client()
+            print(2)
             time.sleep(2)
-            app.deiconify()
+            app.mainloop()
         else:
             print(f"error : {response_content.get('error')}")
     else:
@@ -291,18 +275,23 @@ def wait_for_client():
 def hide_window():
     global app
     app.withdraw()
-    run_icon()
+    time.sleep(5)
+    app.deiconify
 
 
 def main():
     global app
+    init_icon()
+    run_icon()
     wait_for_client()
     time.sleep(1)
-    init_icon()
     app = App()
-    app.protocol('WM_DELETE_WINDOW', hide_window)
+    app.protocol('WM_DELETE+_WINDOW', hide_window)
     app.mainloop()
 
 
 if __name__ == '__main__':
     main()
+
+"""https://stackoverflow.com/questions/26168967/invalid-command-name-while-executing-after-script
+refresh issue"""
